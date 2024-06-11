97c234d4391b028cca58dcf06ff7885b16e11a60
ujala <singhujala2004@gmail.com>
2024-06-11 09:32:57 +0530

    added routes and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './components/Home'
+import About from './components/About'
+import Signin from './components/Signin'
+import SignUp from './components/SignUp'
+import Profile from './components/Profile'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/aboout' element={<About />} />
+          <Route exact path='/sign-in' element={<Signin />} />
+          <Route exact path='/sign-up' element={<SignUp />} />
+          <Route exact path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
   )
 }
 
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-3xl text-red-400'>Home</div>
+  )
+}
+
+export default Home
