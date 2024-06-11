70e66a5c035aa238085a09621a621177b8336b8c
ujala <singhujala2004@gmail.com>
2024-06-11 09:52:01 +0530

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,16 @@ import About from './components/About'
 import Signin from './components/Signin'
 import SignUp from './components/SignUp'
 import Profile from './components/Profile'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />} />
-          <Route exact path='/aboout' element={<About />} />
+          <Route exact path='/about' element={<About />} />
           <Route exact path='/sign-in' element={<Signin />} />
           <Route exact path='/sign-up' element={<SignUp />} />
           <Route exact path='/profile' element={<Profile />} />
