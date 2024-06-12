8d9746f988ee3e266dce003a622fbc73ec5d97d5
ujala <singhujala2004@gmail.com>
2024-06-12 08:45:42 +0530

    "Updated auth controller, added CORS dependency, modified server.js, updated SignUp component, and configured Vite proxy"

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,35 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
+import cors from "cors"
 
 dotenv.config()
 
+mongoose
+   .connect(process.env.MONGODB_URL)
+   .then(() => {
+      console.log('conected to database')
+   })
+   .catch((err) => console.log(err))
 
 const app = express()
-const port = process.env.PORT || 3000
-app.use(express.json())
-
-app.use("/api/users/",userRoutes)
-app.use("/api/auth/",authRoutes)
-
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`listening on port:`, port))
+app.use(express.json())
+// app.use(cors())
 
-mongoose
-    .connect(process.env.MONGODB_URL)
-    .then(() => {
-       console.log('conected to database')
-    })
-    .catch((err)=>console.log(err))
+app.use("/api/users/", userRoutes)
+app.use("/api/auth/", authRoutes)
+
+app.use((err, req, res, next) => {
+   const statusCode = err.statuscode || 501
+   const message  = err.message || "Internal server error"
+  
+   return res.status(statusCode).json({
+      success:false,
+      statusCode,
+      message
+   })
+})
+
+
