26ddb612ce01a6ca3ec037186a550747e3a6fbda
ujala <singhujala2004@gmail.com>
2024-06-17 09:31:18 +0530

    "Updated App.jsx to conditionally render Home or Signin based on currentUser, and refactored About.jsx and Home.jsx to include descriptive text and styling."

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,15 @@ import Signin from './components/Signin'
 import SignUp from './components/SignUp'
 import Profile from './components/Profile'
 import Navbar from './components/Navbar'
-
+import { useSelector } from "react-redux"
 const App = () => {
+  const { currentUser } = useSelector((state) => state.user)
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<Home />} />
+           {currentUser ? <Route exact path='/' element={<Home />} /> : <Route exact path='/' element={<Signin />} />}
           <Route exact path='/about' element={<About />} />
           <Route exact path='/sign-in' element={<Signin />} />
           <Route exact path='/sign-up' element={<SignUp />} />
